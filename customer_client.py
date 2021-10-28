cccf335af65e38dba2a528d8d343f60dc7179750
chrisMsnyder <31325897+chrisMsnyder@users.noreply.github.com>
2021-10-28 11:13:23 -0500

    Add files via upload

--- customer_client.py
+++ customer_client.py
@@ -1,34 +1,55 @@
 import Pyro4
-import uuid
+import argparse
 
 global username
 global password
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', dest='username', action='store')
+    parser.add_argument('-p', dest='password', action='store')
+    parser.add_argument('-shopped', dest='shopped', action='store', default='y')
+    
+    args = parser.parse_args()
+    return args
+
+
 class Customer(object):
-    def __init__(self):
-        self._client_id = uuid.uuid4()
+    def __init__(self, args):
         self.Storefront = Pyro4.Proxy(f"PYRONAME:store.server")
+        self.args = args
 
     def start_shopping(self):
         try:
+            self.Storefront.ping()
             text = ''
             can_enter = False
-            ns = Pyro4.locateNS()
-            while (text.lower() != 'y' and text.lower() != 'n'):
-                text = input('Have you shopped with us before? (y/n)')
-                text = text.lower()
-                if text == 'n':
-                    print('Please create an account')
-                elif text == 'y':
-                    print('Please login')
-                else:
-                    continue
+            user_pass_preset = False
+            username = ''
+            password = ''
+
+            if args.username and args.password:
+                user_pass_preset = True
+            while (text.lower() != 'y' and text.lower() != 'n' and not can_enter):
+                if not user_pass_preset:
+                    text = input('Have you shopped with us before? (y/n)')
+                    text = text.lower()
+                    if text == 'n':
+                        print('Please create an account')
+                    elif text == 'y':
+                        print('Please login')
+                    else:
+                        continue
                 while (not can_enter):
-                    username = input('Username: ')
-                    password = input('Password: ')
+                    if not user_pass_preset:
+                        username = input('Username: ')
+                        password = input('Password: ')
+                    else:
+                        username = args.username
+                        password = args.password
                     is_exists = self.Storefront.check_account_exists(username, password)
                     is_shopping = self.Storefront.check_if_current_shopper(username, password)
-                    if text == 'y':
+                    if text == 'y' or args.shopped.lower() == 'y':
                         if not is_exists:
                             print(f"Sorry, no account exists with Username: '{username}' and Password: '{password}'.\n")
                         else:
@@ -37,7 +58,7 @@ class Customer(object):
                                 can_enter = True
                             else:
                                 print('We are sorry, someone already appears to be logged on with that account. Please try another account.')
-                    if text == 'n':
+                    elif text == 'n' or args.shopped.lower() == 'n':
                         if is_exists:
                             if is_shopping:
                                 print('We are sorry, someone already appears to be logged on with that account. Please try another account.')
@@ -48,6 +69,7 @@ class Customer(object):
                             print('Welcome to the store!\n')
                             self.Storefront.create_account(username, password) 
                             can_enter = True
+                    user_pass_preset = False
             self.Storefront.add_current_customer(username, password)
             while (text != 'exit'):
                 text = input("What would you like to do in the store? ")
@@ -57,16 +79,17 @@ class Customer(object):
                     print('Available options:')
                     print("   'browse' : Tells you whats available in the store")
                     print("   'buy [item name]' : buys the item name from the store\n")
+                    print("   'buy_sleep [item name]' : buys the item name from the store with a 10 sec sleep timer. Used for Testing.\n")
                 if text[0] == 'browse':
-                    print('Checking inventory...')
-                    response = self.Storefront.browse()
-                    [print(x) for x in response]
+                    print('Inventory: ')
+                    response = self.Storefront.get_inventory()
+                    print(response)
                 if text[0] == 'buy':
                     response = self.Storefront.buy_item(text[1], username, password)
                     print(response)
-                # This is a tester function for testing the semaphore. The remote function is idential to buy_item() except it also includes a sleep() function
+                # This is a tester function for testing the semaphore. It includes a sleep function call to delay the purchase
                 if text[0] == 'buy_sleep':
-                    response = self.Storefront.buy_item_sleep(text[1], username, password)
+                    response = self.Storefront.buy_item(text[1], username, password, 10)
                     print(response)
                 if text[0] == 'exit':
                     print('Goodbye!')
@@ -77,9 +100,9 @@ class Customer(object):
 
 if __name__ == '__main__':
     try:
+        args = parse_args()
         print('Entering store...')
-        client = Customer()
+        client = Customer(args)
         client.start_shopping()
     except (KeyboardInterrupt, EOFError):
-        print('Goodbye! (:')
-    
+        print('Goodbye!')
--- store_server.py
+++ store_server.py
@@ -9,50 +9,45 @@ import time
 
 from Pyro5.client import Proxy
 
-items_for_sale = ['bicycle', 'lamp', 'tv']
+items_for_sale = ['bicycle', 'lamp', 'tv', 'tv']
 account_dict = {'admin,admin':{}}
 current_customers = []
-sem = [1]
+item_dict = {}
 
 @Pyro4.expose
 class Store(object):
     def __init__(self):
-        self.sem = Sempaphore()
-        self.items_for_sale = items_for_sale
+        self.item_dict = item_dict
 
-    def browse(self):
-        return items_for_sale
+    def get_inventory(self):
+        inventory = ''
+        for it in self.item_dict.keys():
+            inventory = inventory + it + ": " + str(self.item_dict[it].get_number_in_stock()) + '\n'
+        return inventory
     
-    def buy_item(self, item, username, password):
-        self.sem.wait()
-        account = ','.join([username, password])
-        try:
-            if item in self.items_for_sale:
-                self.items_for_sale.remove(item)
-                print(f"{item} was purchased by {account}!")
-                print(f"Inventory: {items_for_sale}")
-                return f'Enjoy your {item}!'
-            else:
-                return f"We are sorry, '{item}' is not in stock."
-        finally:
-            self.sem.signal()
+    def buy_item(self, item, username, password, sleep=None):
+        if item in self.item_dict.keys():  
+            self.item_dict[item].wait()
 
+            # This is for testing the semaphore.
+            if sleep:
+                time.sleep(sleep)
 
-    # This is a tester function for testing the semaphore. It's idential to buy_item() except it also includes a
-    def buy_item_sleep(self, item, username, password):
-        self.sem.wait()
-        time.sleep(10)
-        account = ','.join([username, password])
-        try:
-            if item in items_for_sale:
-                items_for_sale.remove(item)
-                print(f"{item} was purchased by {account}!")
-                print(f"Inventory: {items_for_sale}")
-                return f'Enjoy your {item}!'
-            else:
-                return f"We are sorry, '{item}' is not in stock."
-        finally:
-            self.sem.signal()
+            account = ','.join([username, password])
+            try:
+                if self.item_dict[item].get_number_in_stock() > 0:
+                    self.item_dict[item].reduce_number_in_stock()
+                    print(f"{item} was purchased by {account}!")
+                    print(f"Inventory: ")
+                    print(self.get_inventory()) 
+                    return f'Enjoy your {item}!'
+                else:
+                    return f"We are sorry, we don't have '{item}' in stock right now."
+            finally:
+                self.item_dict[item].signal()
+        else:
+            return f"We are sorry, we don't sell '{item}'."
+
 
     def check_account_exists(self, username, password):
         if ','.join([username, password]) in account_dict:
@@ -64,13 +59,13 @@ class Store(object):
         account = ','.join([username, password])
         account_dict[account] = {}
         print(f'Customer account has been created. Username: "{username}" and Password: "{password}"')
-        print(f'All accounts: {[x for x in account_dict.keys()]}')
+        print(f'All accounts: {[x for x in account_dict.keys()]}\n')
 
     def add_current_customer(self, username, password):
         account = ','.join([username, password])
         current_customers.append(account)
         print(f'Customer entered the shop: {account}')
-        print(f'Accounts currently shopping: {[x for x in current_customers]}')
+        print(f'Accounts currently shopping: {[x for x in current_customers]}\n')
     
     def check_if_current_shopper(self, username, password):
         account = ','.join([username, password])
@@ -83,7 +78,10 @@ class Store(object):
         account = ','.join([username, password])
         current_customers.remove(account)
         print(f'Customer left the shop: {account}')
-        print(f'Accounts currently shopping: {[x for x in current_customers]}')
+        print(f'Accounts currently shopping: {[x for x in current_customers]}\n')
+
+    def ping(self):
+        return "Customer is entering store."
 
 
 ####################
@@ -92,22 +90,52 @@ class Naming_Server(threading.Thread):
     def run(self):
         os.system("python -m Pyro4.naming")
 
-#####################
 
-class Sempaphore():
-    def __init__(self):
-        self.sem = sem
+#######################
+
+class Item():
+    def __init__(self, item_name):
+        self.sem = 1
+        self.item_name = item_name
+        self.number_in_stock = 1
+
+    def get_item_name(self):
+        return self.item_name
+
+    def get_number_in_stock(self):
+        return self.number_in_stock
+
+    def reduce_number_in_stock(self):
+        self.number_in_stock -= 1
+
+    def increase_number_in_stock(self):
+        self.number_in_stock += 1
 
     def wait(self):
         while(True):
-            if self.sem[0] >= 1:
-                self.sem[0] -= 1
+            if self.sem >= 1:
+                self.sem -= 1
                 break
 
     def signal(self):
-        self.sem[0] += 1
+        self.sem += 1
+    
 
 #######################
+
+def stock_inventory():
+    for item in items_for_sale:
+        if item not in item_dict:
+            item_dict[item] = Item(item)
+        else:
+            item_dict[item].increase_number_in_stock()
+
+def get_initial_inventory():
+        inventory = ''
+        for it in item_dict.keys():
+            inventory = inventory + it + ": " + str(item_dict[it].get_number_in_stock()) + '\n'
+        return inventory
+
 
 def start_server():
     daemon = Pyro4.Daemon()
@@ -116,7 +144,8 @@ def start_server():
     ns.register('store.server', str(uri))
     print(f'\nOpen For Business!')
     print(f"Customer Accounts: {[x for x in account_dict.keys()]}")
-    print(f"Inventory: {items_for_sale}")
+    print(f"Inventory: ")
+    print(get_initial_inventory(),'\n')
     daemon.requestLoop()
 
 
@@ -125,6 +154,7 @@ if __name__ == '__main__':
         print("Starting up server...")
         thread = Naming_Server()
         thread.start()
+        stock_inventory()
         start_server()
     except (KeyboardInterrupt, EOFError):
         print("Sorry We're Closed!")
